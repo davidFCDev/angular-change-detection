f17b66f4a0a12749ef38e13ca4697dbc4e1336f6
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-05 16:09:15 +0200

    onPush

--- src/app/components/on-push/on-push.component.ts
+++ src/app/components/on-push/on-push.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-on-push',
   templateUrl: './on-push.component.html',
-  styleUrls: ['./on-push.component.scss']
+  styleUrls: ['./on-push.component.scss'],
 })
-export class OnPushComponent {
+export class OnPushComponent implements OnInit {
 
+  /**
+   * @description
+   * Valor que se va a incrementar cada segundo en archivo TS
+   * y que dependiendo de la estrategia de Change Detection
+   * se va a actualizar o no en el archivo HTML
+   */
+
+  seconds: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.seconds++;
+      console.log('Segundos transcurridos:', this.seconds);
+    }, 1000);
+  }
 }
