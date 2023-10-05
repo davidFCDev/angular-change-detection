565cefa908039b05dd5a3116381d7587b8167105
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-05 16:22:09 +0200

    lista de nombres aleatorios

--- src/app/components/detach/detach.component.ts
+++ src/app/components/detach/detach.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, Injectable } from '@angular/core';
+import * as Mock from 'mockjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataListProvider {
+
+  get data() {
+    return Mock.mock({
+      'list|10': [{
+        'id|+1': 1,
+        'name': '@cname',
+        'age|20-30': 1,
+        'address': '@county(true)',
+        'checked': false
+      }]
+    }).list;
+  }
+}
 
 @Component({
   selector: 'app-detach',
   templateUrl: './detach.component.html',
-  styleUrls: ['./detach.component.scss']
+  styleUrls: ['./detach.component.scss'],
 })
-export class DetachComponent {
+export class DetachComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
+
+
