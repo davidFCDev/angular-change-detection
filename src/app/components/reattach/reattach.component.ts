4e1d0137321188df00e80f2d05ee514ebd2f909b
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-05 16:45:46 +0200

    reattach ts

--- src/app/components/reattach/reattach.component.ts
+++ src/app/components/reattach/reattach.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Injectable,
+  ChangeDetectorRef,
+} from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class BitcoinPriceProvider {
+  price: number = 100;
+
+  constructor() {
+    setInterval(() => {
+      this.price = Math.floor(Math.random() * 1000) + 100;
+      console.log('Precio actual', this.price);
+    }, 500);
+  }
+}
 @Component({
   selector: 'app-reattach',
   templateUrl: './reattach.component.html',
-  styleUrls: ['./reattach.component.scss']
+  styleUrls: ['./reattach.component.scss'],
+  inputs: ['life'],
 })
-export class ReattachComponent {
+export class ReattachComponent implements OnInit {
+  showLive: boolean = true;
 
+  constructor(
+    public bitcoinPriceProvider: BitcoinPriceProvider,
+    private ref: ChangeDetectorRef
+  ) {}
+
+  set showLiveValue(value: boolean) {
+    this.showLive = value;
+    if (value) {
+      this.ref.reattach();
+    } else {
+      this.ref.detach();
+    }
+  }
+
+  ngOnInit(): void {}
 }
