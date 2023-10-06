1c9080de90d79949a0d92f770661d7515fea459f
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-06 11:45:51 +0200

    nuevo valor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,23 @@ import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngChangeDetection';
 
   live: boolean = true;
 
-  items = [{}]
+  items = [{}];
   items$ = new BehaviorSubject(this.items);
+
+  addItem() {
+    const nuevoItem = Math.floor(Math.random() * 100) + 1;
+
+    this.items.push({
+      numero: nuevoItem,
+    });
+
+    this.items$.next(this.items);
+  }
 }
